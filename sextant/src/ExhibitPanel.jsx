7250e7c979fa6b17bcb00bd97857a20d949c3ae1
germ <29132034+lowell1@users.noreply.github.com>
2022-08-01 17:57:16 -0700

    remove commented code

--- sextant/src/ExhibitPanel.jsx
+++ sextant/src/ExhibitPanel.jsx
@@ -6,14 +6,12 @@ export default function ExhibitPanel({ exhibits }) {
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
   return (
-    // <div className="h-full flex items-center justify-center">
     <div className="border-2 max-w-[1200px] mt-20 mx-auto">
       <div className="divide-x-[2px] flex">
         {exhibits.map((exhibit, index) => (
           <span
             tabIndex="0"
             role="button"
-            // className={selectedTabIndex === index ? "grow " : "grow border-b-2"}
             className={`grow outline-0 ${
               selectedTabIndex === index ? "" : "grow border-b-2"
             }`}
@@ -26,31 +24,5 @@ export default function ExhibitPanel({ exhibits }) {
       </div>
       <div>{exhibits[selectedTabIndex].content}</div>
     </div>
-    // </div>
   );
 }
-// export default function ExhibitPanel({ exhibits }) {
-//   // index of exhibit to display
-//   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-
-//   return (
-//     <div className="h-full flex items-center justify-center">
-//       <div className="border-2 min-w-[50%]">
-//         <div className="divide-x-[2px]">
-//           {exhibits.map((exhibit, index) => (
-//             <span
-//               role="button"
-//               className={`flex justify-between ${
-//                 selectedTabIndex === index ? "" : "border-b-2"
-//               }`}
-//               onClick={() => setSelectedTabIndex(index)}
-//             >
-//               {exhibit.tabLabel}
-//             </span>
-//           ))}
-//         </div>
-//         <div>{exhibits[selectedTabIndex].content}</div>
-//       </div>
-//     </div>
-//   );
-// }
